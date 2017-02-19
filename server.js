c45c03c41edb449c235820b58a958012fd83a285
amanmanit <anupiitd2016@gmail.com>
2017-02-19 23:52:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,16 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Anup Mandal',
-    heading: 'Article One',
-    date: 'Sept 5',
-    content: `  <p>
-                    This is the contect for my first article. This is the contect for my first article. This is the contect for my first article.
-                    This is the contect for my first article. This is the contect for my first article. This is the contect for my first article.
-                </p>`
+var articles = {
+    'article-one' : {
+        title: 'Article One | Anup Mandal',
+        heading: 'Article One',
+        date: 'Sept 5',
+        content: `  <p>
+                        This is the contect for my first article. This is the contect for my first article. This is the contect for my first article.
+                        This is the contect for my first article. This is the contect for my first article. This is the contect for my first article.
+                    </p>`
+    },
+    'article-two' : {title: 'Article Two | Anup Mandal',
+        heading: 'Article Two',
+        date: 'Sept 10',
+        content: `  <p>
+                        This is the contect for my second article. This is the contect for my second article. This is the contect for my second article.
+                        This is the contect for my second article. This is the contect for my second article. This is the contect for my second article.
+                    </p>
+                    <p>
+                        This is the contect for my second article. This is the contect for my second article. This is the contect for my second article.
+                        This is the contect for my second article. This is the contect for my second article. This is the contect for my second article.
+                    </p>                    
+                    `},
+    'article-three' : {title: 'Article Three | Anup Mandal',
+        heading: 'Article Three',
+        date: 'Sept 15',
+        content: `  <p>
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                    </p>
+                    <p>
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                    </p>
+                    <p>
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                        This is the contect for my third article. This is the contect for my third article. This is the contect for my third article.
+                    </p>
+                    `}
 };
-
 function createTemplate (data) {
     var title = data.title;
     var heading = data.heading;
@@ -50,20 +79,14 @@ function createTemplate (data) {
     `;
     return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req,res) {
-    res.send(createTemplate(articleOne)); 
-});
-
-app.get('/article-two', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function(req,res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName])); 
 });
 
 app.get('/ui/style.css', function (req, res) {
