ff2bfb1f70b2c51239a4f6aa9c2644ca4dc879a0
amanmanit <anupiitd2016@gmail.com>
2017-02-21 23:31:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New value';
 
 //move the image 
-var img = document.getElementById('img');
-img.onclick = funtion () {
+var img = document.getElementById('madi');
+img.onclick = function () {
   img.style.marginLeft = '100px';  
 };
