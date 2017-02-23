b5a813f006c6abb974521fb18eebf83c227835db
amanmanit <anupiitd2016@gmail.com>
2017-02-23 19:54:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,8 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function(){
-    //make a req to the counter end point
+    //create req object
     var request = new XMLHttpRequest();
     
     //capture the res and store it in a variable
@@ -26,14 +25,27 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //make a request to the server and send the name
+        //create req object
+    var request = new XMLHttpRequest();
     
-    //capture a list of name and render it as a list 
-    var names = ['name1','name2','name3'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list +='<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    //capture the res and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState == XMLHttpRequest.DONE) {
+          if (request.status == 200) {
+             //capture a list of name and render it as a list 
+                var names = request.responseText;
+                names=JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++) {
+                    list +='<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;          
+          }
+      }  
+    };
+    //make the request
+    request.open('GET','http://amanmanit.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+    //make a request to the server and send the name 
 };
