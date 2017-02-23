940ce5c97f9677fc625a3b26109459b3036dc204
amanmanit <anupiitd2016@gmail.com>
2017-02-23 19:08:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,21 @@ button.onclick = function(){
     request.open('GET','http://amanmanit.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+//submit name 
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function () {
+    //make a request to the server and send the name
+    
+    //capture a list of name and render it as a list 
+    var name = ['name1','name2','name3'];
+    var list = '';
+    for (var i=0; i<name.length; i++) {
+        list +='<li>' + name[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
